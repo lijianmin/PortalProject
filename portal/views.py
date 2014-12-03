da82fbd9f7d3c56ff30710dc16bb7855c20070c7
Jianmin <lijianmin87@hotmail.com>
2014-12-03 15:28:06 +0800

    Second commit

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+
+from portal.models import posts
 
 # Create your views here.
+def home(request):
+	content = {
+        'title' : 'My First Post',
+        'author' : 'Giles',
+        'date' : '18th September 2011',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus tempus dui, ut vulputate nisl eleifend eget. Aenean justo felis, dapibus quis vulputate at, porta et dolor. Praesent enim libero, malesuada nec vestibulum vitae, fermentum nec ligula. Etiam eget convallis turpis. Donec non sem justo.',
+    }
+	return render(request, 'index.html', content)
--- wassuphealth/urls.py
+++ wassuphealth/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from portal import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'wassuphealth.views.home', name='home'),
+    url(r'^$', 'portal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
